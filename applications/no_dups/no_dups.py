b8869e3c1e4f5d5d0ad80be21ba6ab1d18d9c046
hkang42 <h.kang.q@gmail.com>
2020-07-09 23:46:33 -0400

    no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,23 @@
+# I'm pretty sure using a set is better than a dictionary here.
+# Can adapt the solution to just using dictionary.keys if a hash table is required
+
 def no_dups(s):
     # Your code here
+    word_set = set()
+    
+    s_list = s.split()
 
+    output = ''
 
+    for word in s_list:
+        if word in word_set:
+            continue
+        
+        else:
+            word_set.add(word)
+            output += word + ' '
+
+    return output[0:-1]
 
 if __name__ == "__main__":
     print(no_dups(""))
